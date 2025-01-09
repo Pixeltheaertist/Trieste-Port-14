d4a19e23c5581bda544995c4a6f4952360aacb7b
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-01-09 09:55:47 -0800

    Update TheThingyComponent.cs

--- Content.Shared/TP14/Antagonists/TheThingy/TheThingyComponent.cs
+++ Content.Shared/TP14/Antagonists/TheThingy/TheThingyComponent.cs
@@ -1,1 +1,18 @@
 
+using Content.Shared.Dataset;
+using Content.Shared.NPC.Prototypes;
+using Content.Shared.Random;
+using Content.Shared.Roles;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Server.GameTicking.Rules.Components
+{
+    [RegisterComponent]
+    public sealed partial class TheThingyComponent : Component
+    {
+
+        
+    }
+}
