f3be27d631766e2debe7c35ed6d435665f787b92
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-01-09 08:57:19 -0800

    Update and rename nullfile to TheThingyRuleComponent.cs

---
+++ Content.Shared/TP14/Antagonists/TheThingy/TheThingyRuleComponent.cs
@@ -0,0 +1,18 @@
+
+using Content.Shared.Dataset;
+using Content.Shared.NPC.Prototypes;
+using Content.Shared.Random;
+using Content.Shared.Roles;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Content.Shared.Implants;
+
+namespace Content.Server.GameTicking.Rules.Components
+{
+    [RegisterComponent, Access(typeof(TraitorRuleSystem))]
+    public sealed partial class TheThingyRuleComponent : Component
+    {
+    
+    }
+}
