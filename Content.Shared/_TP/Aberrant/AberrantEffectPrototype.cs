ca3a3fe2030d886d7e8dcbce0438d44243ff3542
Lye <128915833+Lyroth001@users.noreply.github.com>
2025-03-12 14:20:22 +0000

    Webedit - Try to make aberrant effects less of a mess

--- Content.Shared/_TP/Aberrant/AberrantEffectPrototype.cs
+++ Content.Shared/_TP/Aberrant/AberrantEffectPrototype.cs
@@ -5,26 +5,31 @@ namespace Content.Shared._TP.Aberrant;
 /// <summary>
 /// This is a prototype for...
 /// </summary>
-[Prototype("aberrantEffect")]
+[Prototype("aberrantEffectCommpound")]
 [DataDefinition]
-public sealed partial class AberrantEffectPrototype : IPrototype
+public sealed partial class AberrantEffectCompoundPrototype : IPrototype
 {
     /// <inheritdoc/>
     [IdDataField]
     public string ID { get; private set;} = default!;
 
     /// <summary>
-    ///     Components making up the effect go here. Also works for compound effects if we wanna be fancy
-    ///     This should be things like shuddering, or other brief effects
+    ///     Stores the effects making up this effect. List incase we want compound effects
     /// </summary>
-    [DataField("temporaryComponents", serverOnly: true)]
-    public ComponentRegistry Components = new();
+    [DataField("effects")]
+    public List<AberrantEffect> Effects = new();
+}
 
-    /// <summary>
-    /// Components that are permanently added to an entity when the effect's node is entered.
-    /// This will be things like that 'madman' antag that was mentioned in discord
-    /// Yes I am stealing a lot of arti code
-    /// </summary>
-    [DataField("permanentComponents")]
-    public ComponentRegistry PermanentComponents = new();
+[Prototype("aberrantEffect")]
+[DataDefinition]
+public sealed partial class AberrantEffectPrototype : IPrototype
+{
+    [IdDatafield]
+    public string ID { get; private set;} = default!;
+
+    ///<summary>
+    ///    Component for an aberrant effect
+    ///</summary>
+    [DataField]
+    public List<Component> Components = new();
 }
