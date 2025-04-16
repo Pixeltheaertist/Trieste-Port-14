8bec97fbf8cfcf1fa6dc577d4e78b579e3924a04
Quantum-cross <7065792+Quantum-cross@users.noreply.github.com>
2025-04-15 20:52:04 -0400

    Remove problematic GameTicker.Player.GetPlayerProfile(ICommonSession) Since a session doesn't have a single "selected character" this had to be changed.
    Now when a player mob is spawned, a copy of the HumanoidCharacterProfile is saved into the HumanoidAppearanceComponent as the original "Base Profile" that created it.
    
    Downstream effects of this:
    
    - Spawn admin verb: use the "Saved Base Profile" instead of currently connected session's "currently selected character"
    - Clone admin verb: use the "Saved Base Profile" instead of currently connected session's "currently selected character"
    - RespawnRuleSystem.RespawnPlayer uses the "Saved Base Profile"
    - SpawnPlayer overload that doesn't take a HumanoidCharacterProfile is stubbed out to just fail. I'm not sure how to handle this as I'm not exactly sure which code paths get here...
    - Joining as observer uses your username not your character name (which one...?)

--- Content.Shared/Humanoid/HumanoidAppearanceComponent.cs
+++ Content.Shared/Humanoid/HumanoidAppearanceComponent.cs
@@ -2,6 +2,7 @@ using Content.Shared.DisplacementMap;
 using Content.Shared.Humanoid.Markings;
 using Content.Shared.Humanoid.Prototypes;
 using Content.Shared.Inventory;
+using Content.Shared.Preferences;
 using Robust.Shared.Enums;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
@@ -106,6 +107,8 @@ public sealed partial class HumanoidAppearanceComponent : Component
     /// </summary>
     [DataField]
     public Dictionary<HumanoidVisualLayers, DisplacementData> MarkingsDisplacement = new();
+
+    public HumanoidCharacterProfile? BaseProfile;
 }
 
 [DataDefinition]
