81110affd35a0f23667bec8690f575f74ad1d656
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-04-28 12:39:19 -0700

    Create MoneyVaultSystem.cs

---
+++ Content.Server/_TP/Pirates/MoneyVaultSystem.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics.CodeAnalysis;
+using Content.Server.Cargo.Components;
+using Content.Server.Labels.Components;
+using Content.Server.Station.Components;
+using Content.Shared.Cargo;
+using Content.Shared.Cargo.BUI;
+using Content.Shared.Cargo.Components;
+using Content.Shared.Cargo.Events;
+using Content.Shared.Cargo.Prototypes;
+using Content.Shared.Database;
+using Content.Shared.Emag.Components;
+using Content.Shared.IdentityManagement;
+using Content.Shared.Interaction;
+using Content.Shared.Paper;
+using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Server.Cargo.Systems
+{
+    public sealed partial class CargoSystem
+    {
+        [Dependency] private readonly SharedTransformSystem _transformSystem = default!;
+
+        /// <summary>
+        /// Keeps track of how much time has elapsed since last balance increase.
+        /// </summary>
+        private float _timer;
+
+        private void InitializeConsole()
+        {
+            SubscribeLocalEvent<MoneyVaultComponent, InteractUsingEvent>(OnInteractUsing);
+            Reset();
+        }
+
+        private void OnInteractUsing(EntityUid uid, MoneyVaultComponent component, ref InteractUsingEvent args)
+        {
+            if (!HasComp<CashComponent>(args.Used))
+                return;
+
+            var price = _pricing.GetPrice(args.Used);
+
+            if (price == 0)
+                return;
+
+            component.VaultAmount = price;
+            QueueDel(args.Used);
+        }
+    }
+}
