2f50f7e0bc6a77e5e1ca924e7042ef106b3af5ad
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-05-07 09:45:22 -0700

    Update FallingDestinationComponent.cs

--- Content.Server/Falling/FallingDestinationComponent.cs
+++ Content.Server/Falling/FallingDestinationComponent.cs
@@ -1,5 +1,9 @@
 namespace Content.Server.Falling;
 
+
+//Summary
+// This component dictates what a "FallingDestination" is. Basically, where is the general area that entities will fall to? There should only be one per game.
+//Summary
 [RegisterComponent]
 public sealed partial class FallingDestinationComponent : Component
 {
