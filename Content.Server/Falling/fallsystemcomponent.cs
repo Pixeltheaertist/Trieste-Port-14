f0eb5e97e50cc6391dce80d96b5f58a110923897
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-05-07 09:46:00 -0700

    Update fallsystemcomponent.cs

--- Content.Server/Falling/fallsystemcomponent.cs
+++ Content.Server/Falling/fallsystemcomponent.cs
@@ -1,5 +1,8 @@
 namespace Content.Server.Falling;
 
+//Summary
+// Anything with this component will be affected by Trieste gravity and falling, see FallSystem.cs.
+//Summary
 [RegisterComponent]
 public sealed partial class FallSystemComponent : Component
 {
