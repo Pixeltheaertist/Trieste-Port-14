54e3748973cd83e9827379e12ee6dbe179cd5834
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-11-24 11:17:35 -0800

    Update ThunderSystem.cs

--- Content.Server/_TP/ThunderSystem.cs
+++ Content.Server/_TP/ThunderSystem.cs
@@ -87,23 +87,19 @@ public sealed class ThunderSystem : EntitySystem
                 {
                     return;
                 }
-                LightningSound = entity.LightningSoundPrototype;
 
                 if (entity.StormMode) // If marker is currently in a "Flash Storm"
                 {
                     LightningType = entity.StormStrikePrototype;
-                    LightningSound = entity.LightningSoundPrototype;
                     var strikeChance = _random.Prob(0.3f); // Roll a =30% chance for lightning to strike
                     if (strikeChance)
                     {
                         LightningType = entity.StormLightningPrototype;
-                        LightningSound = entity.LightningSoundPrototype;
                         //Log.Error("striking lightning fr fr");// Change lightning prototype to a strike prototype
                     }
                 }
 
                 Spawn(LightningType, newCoords); // Spawn lightning prototype
-                Spawn(LightningSound, newCoords); // And sound too
             }
 
             _updateTimer = 0; // Reset lightning timer
