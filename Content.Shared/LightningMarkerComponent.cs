2db83f140eed753b9f2ee480912d2de297edc94f
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-11-24 11:15:16 -0800

    Update LightningMarkerComponent.cs

--- Content.Shared/LightningMarkerComponent.cs
+++ Content.Shared/LightningMarkerComponent.cs
@@ -10,12 +10,15 @@ namespace Content.Shared.Gravity
         [DataField("lightningPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
         public string LightningPrototype = "AdminInstantEffectThunder";
 
+        // The sound object prototype this marker should spawn
         [DataField("lightningSoundPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
         public string LightningSoundPrototype = "AdminInstantEffectThunderSound";
 
+        // The lightning prototype this marker should spawn during flash storms
         [DataField("stormLightningPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
         public string StormLightningPrototype = "Eldritch";
 
+        // The lightning prototype this marker should spawn if the lightning 'strikes' during a flash storm.
         [DataField("stormStrikePrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
         public string StormStrikePrototype = "Storm";
 
@@ -24,12 +27,13 @@ namespace Content.Shared.Gravity
         public float ThunderRange = 70f;
 
         [DataField("cleared")]
-        public bool Cleared = false;
+        public bool Cleared = false; // Used by weather systems. Dictates whether this marker will create lightning or not. False = yes.
 
         // How often (in seconds) the lightning will occur
         [DataField("thunderFrequency")]
         public float ThunderFrequency = 8f;
 
+        // Decides whether this marker will be using the normal LightningPrototype or the StormLightningPrototype
         [DataField("stormMode")]
         public bool StormMode = false;
     }
