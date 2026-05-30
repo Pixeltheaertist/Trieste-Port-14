cda077b08efbe789203426ad41da0ea897d723c2
Lye <zac@doodle.co.uk>
2026-05-30 19:25:14 +0100

    fucking work

---
+++ Content.Shared/_TP/Aberrant/AberrantEffectPrototype.cs
@@ -0,0 +1,21 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Shared._TP.Aberrant;
+
+/// <summary>
+/// This is a prototype for...
+/// </summary>
+[Prototype("aberrantEffect")]
+[DataDefinition]
+public sealed partial class AberrantEffectPrototype : IPrototype
+{
+    /// <inheritdoc/>
+    [IdDataField]
+    public string ID { get; private set;} = default!;
+
+    ///<summary>
+    ///    Component for an aberrant effect
+    ///</summary>
+    [DataField]
+    public List<Component> Components = new();
+}
