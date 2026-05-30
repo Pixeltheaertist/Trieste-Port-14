8901252606767747ff2cf46ec3dd3062278f0e93
DaCookieCakes <108630416+DaCookieCakes@users.noreply.github.com>
2026-05-29 20:32:01 -0700

    Jellid Tweaks MOAR Part 15900
    * Re-add passive drain without gloves.
    * Organize the Update method into methods.
    * Add better documentation.
    * Touched up the Jellid guidebook.

--- Content.Server/_TP/Jellids/JellidDrawSystem.cs
+++ Content.Server/_TP/Jellids/JellidDrawSystem.cs
@@ -6,6 +6,7 @@ using Content.Shared.Damage;
 using Content.Shared.Damage.Systems;
 using Content.Shared.DoAfter;
 using Content.Shared.Electrocution;
+using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Interaction.Events;
 using Content.Shared.Inventory;
 using Content.Shared.Popups;
@@ -18,6 +19,10 @@ using Robust.Shared.Timing;
 
 namespace Content.Server._TP.Jellids;
 
+/// <summary>
+///     The JellidComponent system handling everything related to power.
+///     Such as charging, draining, and alerts.
+/// </summary>
 public sealed class JellidDrawSystem : EntitySystem
 {
     [Dependency] private readonly AlertsSystem _alerts = default!;
@@ -25,11 +30,13 @@ public sealed class JellidDrawSystem : EntitySystem
     [Dependency] private readonly BatterySystem _battery = default!;
     [Dependency] private readonly DamageableSystem _damageable = default!;
     [Dependency] private readonly DoAfterSystem _doAfter = default!;
+    [Dependency] private readonly SharedHandsSystem _hands = default!;
     [Dependency] private readonly InventorySystem _inventory = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly TagSystem _tag = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
 
+    // The jellid-proof gloves tag proto ID.
     private static readonly ProtoId<TagPrototype> FireproofTag = "PreventsFire";
 
     // Track the previous charge to detect if this Jellid is charging.
@@ -42,6 +49,7 @@ public sealed class JellidDrawSystem : EntitySystem
         SubscribeLocalEvent<JellidComponent, ComponentShutdown>(OnShutdown);
         SubscribeLocalEvent<JellidComponent, ElectrocutedEvent>(OnElectrocution);
 
+        // Proper charging.
         SubscribeLocalEvent<BatteryComponent, UseInHandEvent>(OnUseBatteryInHand);
         SubscribeLocalEvent<JellidComponent, JellidBatteryDoAfterEvent>(OnJellidDoAfter);
     }
@@ -136,45 +144,105 @@ public sealed class JellidDrawSystem : EntitySystem
         var query = EntityQueryEnumerator<JellidComponent>();
         while (query.MoveNext(out var uid, out var comp))
         {
+            // Timing check so this doesn't run every tick.
             if (_timing.CurTime < comp.NextPowerDrain)
                 continue;
 
             comp.NextPowerDrain = _timing.CurTime + TimeSpan.FromSeconds(1f);
 
             if (!TryComp<BatteryComponent>(uid, out var jellidBattery))
-                return;
+                continue;
 
-            const float alertChange = 300f;
-            var chargePercent = (short) MathF.Round(jellidBattery.CurrentCharge / jellidBattery.MaxCharge * 10f);
-            if (jellidBattery.CurrentCharge > alertChange)
+            // Alert check!
+            // If the internal battery is below 300, display an empty battery alert.
+            // Otherwise, display a battery alert with the charge percentage.
+            UpdateAlerts(uid, comp, jellidBattery);
+
+            // Held item check!
+            // If the user DOES NOT have gloves on and a battery is held, it will slowly drain into the Jellid.
+            UpdateHeldBatteries(uid, jellidBattery);
+
+            // Damage check!
+            // If the internal battery is below 20, damage the Jellid and add it to a previous charge dict.
+            // We only damage the Jellid if it's NOT charging. we deal 1 slash damage.
+            UpdatePowerDamage(uid, jellidBattery);
+        }
+    }
+
+    /// <summary>
+    ///     Helper method to damage the player if their charge is below 100.
+    ///     NOTE: Needs testing if 100 is too high!
+    /// </summary>
+    /// <param name="uid">Jellid's UID</param>
+    /// <param name="jellidBattery">Jellid's BatteryComponent</param>
+    private void UpdatePowerDamage(EntityUid uid, BatteryComponent jellidBattery)
+    {
+        const float damageCharge = 100f;
+        if (jellidBattery.CurrentCharge >= damageCharge)
+        {
+            _previousCharges[uid] = jellidBattery.CurrentCharge;
+            return;
+        }
+
+        var isCharging = _previousCharges.TryGetValue(uid, out var prevCharge) && jellidBattery.CurrentCharge > prevCharge;
+        if (isCharging)
+            return;
+
+        var damage = new DamageSpecifier
+        {
+            DamageDict = { ["Slash"] = 1f }
+        };
+        _damageable.TryChangeDamage(uid, damage, origin: uid);
+    }
+
+    /// <summary>
+    ///     Helper method to drain HELD batteries passively into the player.
+    ///     This is separate from 'eating' power, but it still feeds them.
+    /// </summary>
+    /// <param name="uid">Jellid's UID</param>
+    /// <param name="jellidBattery">Jellid's BatteryComponent</param>
+    private void UpdateHeldBatteries(EntityUid uid, BatteryComponent jellidBattery)
+    {
+        var hasFireproofGloves = _inventory.TryGetSlotEntity(uid, "gloves", out var glovesUid)
+                                 && _tag.HasTag(glovesUid.Value, FireproofTag);
+
+        if (!hasFireproofGloves)
+        {
+            foreach (var hand in _hands.EnumerateHands(uid))
             {
-                _alerts.ClearAlert(uid, comp.NoBatteryAlert);
-                _alerts.ShowAlert(uid, comp.BatteryAlert, chargePercent);
+                if (!_hands.TryGetHeldItem(uid, hand, out var heldItem))
+                    continue;
+
+                if (!TryComp<BatteryComponent>(heldItem, out var batteryComp))
+                    continue;
+
+                // Drain at a rate of a constant 2.5 power.
+                _battery.SetCharge(heldItem.Value, batteryComp.CurrentCharge - 2.5f);
+                _battery.SetCharge(uid, jellidBattery.CurrentCharge + 2.5f);
             }
-            else
-            {
-                _alerts.ClearAlert(uid, comp.BatteryAlert);
-                _alerts.ShowAlert(uid, comp.NoBatteryAlert);
-            }
+        }
+    }
 
-            const float damageCharge = 20f;
-            if (jellidBattery.CurrentCharge >= damageCharge)
-            {
-                _previousCharges[uid] = jellidBattery.CurrentCharge;
-                return;
-            }
-
-            // Only damage if the Jellid is NOT charging
-            var isCharging = _previousCharges.TryGetValue(uid, out var prevCharge) && jellidBattery.CurrentCharge > prevCharge;
-
-            if (isCharging)
-                return;
-
-            var damage = new DamageSpecifier
-            {
-                DamageDict = { ["Slash"] = 1f }
-            };
-            _damageable.TryChangeDamage(uid, damage, origin: uid);
+    /// <summary>
+    ///     Helper method to update the Jellid's battery alerts.
+    ///     If below 300, an empty battery is displayed. Otherwise, display a numbered 10-0 battery.
+    /// </summary>
+    /// <param name="uid">Jellid's UID</param>
+    /// <param name="comp">Jellid's JellidComponent</param>
+    /// <param name="jellidBattery">Jellid's BatteryComponent</param>
+    private void UpdateAlerts(EntityUid uid, JellidComponent comp, BatteryComponent jellidBattery)
+    {
+        const float alertChange = 300f;
+        var chargePercent = (short) MathF.Round(jellidBattery.CurrentCharge / jellidBattery.MaxCharge * 10f);
+        if (jellidBattery.CurrentCharge > alertChange)
+        {
+            _alerts.ClearAlert(uid, comp.NoBatteryAlert);
+            _alerts.ShowAlert(uid, comp.BatteryAlert, chargePercent);
+        }
+        else
+        {
+            _alerts.ClearAlert(uid, comp.BatteryAlert);
+            _alerts.ShowAlert(uid, comp.NoBatteryAlert);
         }
     }
 }
