69edb1e30fd00eee66620c0b50c10f1e797c6db4
DaCookieCakes <108630416+DaCookieCakes@users.noreply.github.com>
2026-09-18 11:35:03 -0700

    pref unavailable
    dammit

---
+++ Content.Server.Database/Migrations/Postgres/20260918183159_RestorePreferenceUnavailable.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    public partial class RestorePreferenceUnavailable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE profile ADD COLUMN IF NOT EXISTS pref_unavailable integer NOT NULL DEFAULT 0;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE profile DROP COLUMN IF EXISTS pref_unavailable;");
+        }
+    }
+}
